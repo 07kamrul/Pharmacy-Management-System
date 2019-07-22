2018f54b44485b1d345fc25d4559c87f8af4316a
Md Kamrul Hasan <mdkamrulhasanewu@gmail.com>
2019-07-22 18:40:24 +0600

    Add files via upload

--- PharmacyManagementSystem/src/main/java/PharmacyManagementSystem/Controller/MedicineController.java
+++ PharmacyManagementSystem/src/main/java/PharmacyManagementSystem/Controller/MedicineController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import PharmacyManagementSystem.Entity.Category;
@@ -58,7 +59,8 @@ public class MedicineController {
 	}
 
 	@RequestMapping(value = "/saveMedicine", method = RequestMethod.POST)
-	public String saveMedicineInfo(@ModelAttribute("medicine") Medicine medicine) {
+	public String saveMedicineInfo(Model model, @RequestParam("files") MultipartFile[] files,
+			@ModelAttribute("medicine") Medicine medicine) {
 		medicineService.saveMedicine(medicine);
 		return "redirect:/";
 	}
